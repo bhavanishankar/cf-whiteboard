a0fe3c0cb37f560674f468d4700b2611d40b4005
bshankar <bhavanishankar.j@imaginea.com>
2012-11-20 18:45:42 +0530

    image path is made dynamic

--- static/js/jquery.ui.toolbar.js
+++ static/js/jquery.ui.toolbar.js
@@ -10,7 +10,7 @@
             var self = this,
                 o = self.options,
                 el = self.element,
-                toolbar = $(el).append('<div class="tool-bar"><div class="box-title">Tools</div>' + this.getShapesMarkup(o.shapes) + '</div>');
+                toolbar = $(el).append('<div class="tool-bar"><div class="box-title">Tools</div>' + this.getShapesMarkup(o.imagesPath,o.shapes) + '</div>');
 
             $('.shape_icon').live('click', function(event) {
                 if (toolbar.currentShape) {
@@ -19,24 +19,25 @@
                 $('#freeow').show();
                 toolbar.currentShape = $(this);
                 toolbar.currentShape.addClass('icon_selected');
+                event.shapeSelected =  $(this).attr('id');
                 self._trigger('shapeSelected', event);
 
-            })
+            });
             o.dropElement.click(function (e) {
                if (toolbar.currentShape) {
-                   self._trigger('dropElementClicked', e)
+                   self._trigger('dropElementClicked', e);
                    toolbar.currentShape.removeClass('icon_selected');
                }
-            })
+            });
             this.option( this.options );
         },
 
-        getShapesMarkup:function(shapes) {
+        getShapesMarkup:function(imagesPath, shapes) {
             var toolbarShapesMarkup = '';
             for (var index in shapes) {
                 if (shapes.hasOwnProperty(index)) {
                     var _shape = shapes[index].name;
-                    var _shapeSource = "static/images/" + _shape + "_g.png"
+                    var _shapeSource =  imagesPath + shapes[index].iconName;
                     toolbarShapesMarkup += '<div class="shape_icon" id="' + _shape + '"><a href="#" rel="tooltip" title="' + _shape + '"><img alt="' + _shape + '" class="image_style"  src="' + _shapeSource + '" /></a></div><hr />';
                 }
             }
@@ -54,7 +55,6 @@
         _setOption: function(option, value) {
             $.Widget.prototype._setOption.apply( this, arguments );
             var el = this.element;
-
-        }
+         }
     });
 })(jQuery);
