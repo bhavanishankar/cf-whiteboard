c9350fcfef2dfef980d4e411d03c0771ee484bc0
bshankar <bhavanishankar.j@imaginea.com>
2012-11-22 15:09:52 +0530

    updated to call widget methods

--- static/js/sockJSClient.js
+++ static/js/sockJSClient.js
@@ -1,20 +1,21 @@
 var sockJSClient = {
     sockjs_url: '/echo',
     init: function () {
-        app.sockjs = new SockJS(this.sockjs_url);
-        app.sockjs.onopen = this.onSocketOpen;
-        app.sockjs.onmessage = this.onSocketMessage;
-        app.sockjs.onclose = this.onSocketClose;
+        whiteboardApp.sockJS = new SockJS(this.sockjs_url);
+        whiteboardApp.sockJS.onopen = this.onSocketOpen;
+        whiteboardApp.sockJS.onmessage = this.onSocketMessage;
+        whiteboardApp.sockJS.onclose = this.onSocketClose;
     },
 
     onSocketOpen: function () {
         $('#spinner').hide();
         $('#wait').hide();
-        chat.displayMessage('[*] open', app.sockjs.protocol);
-        app.sockjs.send(JSON.stringify({
+       // chat.displayMessage('[*] open', whiteboardApp.sockJS.protocol);
+        whiteboardApp.chat.data('chatwindow').displayMessage('<b>[*] opened :</b> ', whiteboardApp.sockJS.protocol);
+        whiteboardApp.sockJS.send(JSON.stringify({
             action: 'text',
             message: 'Joined',
-            userName: app.userName
+            userName: whiteboardApp.userName
         }));
     },
 
@@ -22,26 +23,26 @@ var sockJSClient = {
         var data = JSON.parse(e.data);
         switch (data.action) {
             case 'text':
-                app.textMessage(data);
+                whiteboardApp.textMessage(data);
             break;
             case 'new_shape':
-                app.createNewShape(data);
+                whiteboardApp.createNewShape(data);
             break;
             case 'modified':
-                canvasObj.modifyObject(data);
+                whiteboardApp.canvas.modifyObject(data);
             break;
             case 'deleted':
-                canvasObj.deleteObject(data);
+                whiteboardApp.canvas.deleteObject(data);
             break;    
         }
     },
 
     onSocketClose: function () {
-        chat.displayMessage('[*] close', '');
-        app.sockjs.send(JSON.stringify({
+        whiteboardApp.chat.data('chatwindow').displayMessage('<b>[ [*] closed ]:</b> ');
+        whiteboardApp.sockJS.send(JSON.stringify({
             action: 'text',
             message: 'Left',
-            userName: app.userName
+            userName: whiteboardApp.userName
         }));
     }
 
